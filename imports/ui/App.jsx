5a1f1cddb2601c2b7d5e4dcd5070e56ae75fd6b8
Christopher Conlan <christopher.conlan15@imperial.ac.uk>
2017-06-30 14:09:51 +0100

    first commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'; 
+import {Grid, Row, Col} from 'react-bootstrap'; 
+
+export default class App extends Component { 
+    render() { 
+        return ( 
+            <h1> hello </h1> 
+        )
+    }
+}
